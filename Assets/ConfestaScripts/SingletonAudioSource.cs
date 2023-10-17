a9e8fb3c7224a86f3da79a52aed802c41c3fc660
gh1928 <oos1928@gmail.com>
2023-10-17 20:49:40 +0900

    텍스트 추가

--- Assets/ConfestaScripts/SingletonAudioSource.cs
+++ Assets/ConfestaScripts/SingletonAudioSource.cs
@@ -6,16 +6,48 @@ public class SingletonAudioSource : MonoBehaviour
 {
     public static SingletonAudioSource Instance { get; private set; }
     private AudioSource audioSource;
+    private Coroutine clipChangeCoroutine;
+
+    public float clipChangeTime = 1f;
+    public float clipChangeDelay = 0.5f;
     private void Awake()
     {
         Instance = this;
         audioSource = GetComponent<AudioSource>();
     }
     public static void PlayClip(AudioClip clip)
+    { 
+        Instance.InstancedPlayClip(clip);
+    }
+
+    public void InstancedPlayClip(AudioClip clip)
     {
-        var audioSource = Instance.audioSource;
+        if(clipChangeCoroutine != null) 
+            StopCoroutine(clipChangeCoroutine);
+
+        clipChangeCoroutine = StartCoroutine(ClipChangeCoroutine(clip));
+    }
+
+    private IEnumerator ClipChangeCoroutine(AudioClip clip)
+    {
+        float sourVolume = audioSource.volume;
+
+        float timer = 0f;
+        float reverseCT = 1 / clipChangeTime;
+
+        while(timer < clipChangeTime)
+        {
+            timer += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(sourVolume, 0f, timer * reverseCT);
+
+            yield return null;
+        }
+
+        //yield return new WaitForSeconds(clipChangeDelay);
+
         audioSource.Stop();
         audioSource.clip = clip;
+        audioSource.volume = 1f;
         audioSource.Play();
     }
 }
--- Assets/ConfestaScripts/TriggerController.cs
+++ Assets/ConfestaScripts/TriggerController.cs
@@ -6,7 +6,7 @@ public class TriggerController : MonoBehaviour
 {
     private PlayerDistanceChecker distanceChecker;
 
-    public Animator animator;
+    private Animator animator;
     private int hashStart = Animator.StringToHash("Start");
 
     public float startDistance = 4.5f;
@@ -15,16 +15,23 @@ public class TriggerController : MonoBehaviour
     private bool isPlaying = false;
     public AudioClip audioClip;
 
+    private CanvasGroup canvasGroup;
+
     private void Awake()
     {
-        distanceChecker = GetComponent<PlayerDistanceChecker>();        
+        distanceChecker = GetComponent<PlayerDistanceChecker>();
+        animator = GetComponentInChildren<Animator>();
         SqrSD = startDistance * startDistance;
+        canvasGroup = GetComponentInChildren<CanvasGroup>();
     }
     private void Update()
     {
         if (distanceChecker.SqrDist > SqrSD)
         {
             isPlaying = false;
+
+            if(canvasGroup  != null) 
+                canvasGroup.alpha = 0f;
             return;
         }
 
@@ -34,8 +41,11 @@ public class TriggerController : MonoBehaviour
 
             animator.SetTrigger(hashStart);
 
-            if(audioClip != null)
+            if (canvasGroup != null)
+                canvasGroup.alpha = 1f;
+
+            if (audioClip != null)
                 SingletonAudioSource.PlayClip(audioClip);
         }
-    }
+    }  
 }
