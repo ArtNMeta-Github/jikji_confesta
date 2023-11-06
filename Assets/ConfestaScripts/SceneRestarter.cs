d90e75e803cfc8ca3b381e05c6659b3d48889cdd
gh1928 <oos1928@gmail.com>
2023-11-06 17:44:02 +0900

    no message

--- Assets/ConfestaScripts/SceneRestarter.cs
+++ Assets/ConfestaScripts/SceneRestarter.cs
@@ -1,3 +1,4 @@
+using Oculus.Interaction;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.ComTypes;
@@ -9,20 +10,25 @@ public class SceneRestarter : MonoBehaviour
     private float timer = 0f;
 
     public float idleTime = 60f;
+
+    private Vector3 lastPos = Vector3.zero;
     void Update()
     {
         if (Input.anyKey)
         {
+            timer = 0f;            
+            return;
+        }
+
+        if(BNG.InputBridge.Instance.LeftThumbstickAxis != Vector2.zero)
+        {
             timer = 0f;
-            Debug.Log("Input");
             return;
         }
 
         timer += Time.deltaTime;
         if (timer >= idleTime)
-            RestartScene();
-
-        Debug.Log(timer);
+            RestartScene();        
     }
 
     void RestartScene()
