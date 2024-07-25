37352f29d84e2ad3b28a4ed93497787ebedf09cc
HwangDongJun <ojb0012@naver.com>
2024-07-25 13:57:27 +0900

    workingMan Prefab create

--- Assets/ConfestaScripts/SceneRestarter.cs
+++ Assets/ConfestaScripts/SceneRestarter.cs
@@ -7,30 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneRestarter : MonoBehaviour
 {
-    private float timer = 0f;
-
-    public float idleTime = 60f;
-
-    private Vector3 lastPos = Vector3.zero;
-    void Update()
-    {
-        if (Input.anyKey)
-        {
-            timer = 0f;            
-            return;
-        }
-
-        if(BNG.InputBridge.Instance.LeftThumbstickAxis != Vector2.zero)
-        {
-            timer = 0f;
-            return;
-        }
-
-        timer += Time.deltaTime;
-        if (timer >= idleTime)
-            RestartScene();        
-    }
-
     public void RestartScene()
     {    
         Scene scene = SceneManager.GetActiveScene();
